f78092ba8cb776996317c8a642e809a96d598df9
peterkarnaukhsite <peterkarnaukh@gmail.com>
2021-12-13 22:31:03 +0200

    page structure added

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app-main-layout">
+    <navbar @click="isOpen = !isOpen" />
+
+    <sidebar v-model="isOpen" />
+
+    <main class="app-content" :class="{ full: !isOpen }">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/app/Navbar";
+import Sidebar from "@/components/app/Sidebar";
+
+export default {
+  name: "main-layout",
+  components: {
+    Navbar,
+    Sidebar,
+  },
+  data() {
+    return {
+      isOpen: true,
+    };
+  },
+};
+</script>
