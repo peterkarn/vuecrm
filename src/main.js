ee77f78b22cd2238ab003475c545994cf2cb9615
peterkarnaukhsite <peterkarnaukh@gmail.com>
2021-12-17 20:22:39 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Vuelidate from 'vuelidate'
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import dateFilter from '@/filters/date.filter'
 import 'materialize-css/dist/js/materialize.min'
 
+
 Vue.config.productionTip = false;
+Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
 
 new Vue({
   router,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,52 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" class="validate">
+        <input
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{
+            invalid:
+              ($v.email.$dirty && !$v.email.required) ||
+              ($v.email.$dirty && !$v.email),
+          }"
+        />
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small
+          v-if="$v.email.$dirty && !$v.email.required"
+          class="helper-text invalid"
+          >Email не должен быть пустым</small
+        >
+        <small
+          v-else-if="$v.email.$dirty && !$v.email.email"
+          class="helper-text invalid"
+          >Введите корректный email</small
+        >
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate">
+        <input
+          id="password"
+          type="password"
+          v-model="password"
+          :class="{
+            invalid:
+              ($v.password.$dirty && !$v.password.required) ||
+              ($v.password.$dirty && !$v.password.minLength),
+          }"
+        />
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+          v-if="$v.password.$dirty && !$v.password.required"
+          class="helper-text invalid"
+          >Поле обязательно для заполнения</small
+        >
+        <small
+          v-else-if="$v.password.$dirty && !$v.password.minLength"
+          class="helper-text invalid"
+          >Пароль должен содержать более 6 символов.</small
+        >
       </div>
     </div>
     <div class="card-action">
@@ -23,8 +59,38 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
+<script>
+import { email, required, minLength } from "vuelidate/lib/validators";
+
+export default {
+  name: "login",
+  data: () => ({
+    email: "",
+    password: "",
+  }),
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(6) },
+  },
+  methods: {
+    submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      const formData = {
+        email: this.email,
+        password: this.password,
+      };
+
+      console.log(formData);
+      // this.$routre.push("/");
+    },
+  },
+};
+</script>
